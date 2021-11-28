ea9671a8606102d4c2f4aea96d8e473ae05ed88d
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2021-11-28 00:56:49 -0300

    add folder backend

---
+++ backend/routes/logado.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const VeiculoController = require('../controllers/VeiculoController')
+//USUARIO
+router.get("/", VeiculoController.findAll)
+router.get("/find", VeiculoController.findByBody)
+router.get("/:id", VeiculoController.findById)
+router.post("/", VeiculoController.post)
+router.put("/:id", VeiculoController.put)
+router.delete("/delete/:id", VeiculoController.delete)
+
+router.get('/',(req, res) => { return res.redirect('/veiculos')})
+
+module.exports = router; 
---
+++ frontend/main.js
@@ -0,0 +1,64 @@
+const url = "http://localhost:3001/veiculos"
+const newVeiculo = {
+    veiculo: "onix",
+    marca_id: 3,
+     ano: 2020,
+      descricao: "feito de papel",
+       vendido: "false"
+}
+
+const VeiculoUpdate = {
+    veiculo: "sandero",
+    marca_id: 3,
+     ano: 2020,
+      descricao: "feito de papel",
+       vendido: "false"
+}
+
+function getVeiculos(){
+    axios.get(url)
+    .then(response => {
+        const data = response.data
+        console.log(response);
+        renderResult.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
+}
+// getVeiculos()
+
+function addNewVeiculo(){
+    axios.post(url, newVeiculo)
+        .then(response => {
+            alert(JSON.stringify(response.data))})
+        .catch(error => console.log(error))
+}
+
+
+function updateVeiculo(){
+    axios.put(url+"/16", VeiculoUpdate)
+    .then(response => {
+        alert(JSON.stringify(response.data))
+    })
+    .catch(error => console.log(error))
+}
+// updateVeiculo()
+
+function deleteVeiculo(){
+    axios.delete(url+"/delete/16")
+    .then(response => {
+        alert(JSON.stringify(response.data))
+    })
+    .catch(error => console.log(error))
+}
+// deleteVeiculo()
+
+function getByIdVeiculos(){
+    axios.get(`${url}/21`)
+    .then(response => {
+        const data = response.data
+        console.log(response);
+        renderResult.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
+}
+getByIdVeiculos()
